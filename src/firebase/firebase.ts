dc656cc60ca28f9c0632fe432c85557a4386fe89
harshath <harshathmkulal@gmail.com>
2024-10-09 14:49:52 +0530

    minor change

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,16 +1,13 @@
-
 import { initializeApp } from "firebase/app";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA_rdASP9ArLe88XayQlajo2bga8txU4gA",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "xike-37f99.firebaseapp.com",
   projectId: "xike-37f99",
   storageBucket: "xike-37f99.appspot.com",
   messagingSenderId: "592669521651",
   appId: "1:592669521651:web:9be380a6214396b56ea986",
-  measurementId: "G-5728SJ2X46"
+  measurementId: "G-5728SJ2X46",
 };
-
 
 export const app = initializeApp(firebaseConfig);
